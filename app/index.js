492902bf12ddd42aff65c8d75504159d629eac94
nongzhongliang <770716377@qq.com>
2017-05-16 11:27:50 +0800

    webpack00

---
+++ app/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+ReactDOM.render((
+  <div>hello world</div>
+), document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+var webpack = require('webpack')
+var path= require('path')
+var HtmlWebpackPlugin= require('html-webpack-plugin')
+module.exports ={
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,loader: 'style-loader!css-loader'
+      },
+      {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+            query: {
+                presets: ['es2015','react']
+            }
+        }
+    ]
+  },
+  resolve: {
+    extensions: ['','.coffee','.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({     // Create HTML file that includes references to bundled CSS and JS.
+      template: 'app/index.html',
+      title: '开发模式',
+      favicon:'./app/favicon.ico',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true
+      },
+      hash:true,
+      // 这样每次客户端页面就会根据这个hash来判断页面是否有必要刷新
+      // 在项目后续过程中，经常需要做些改动更新什么的，一但有改动，客户端页面就会自动更新！
+      inject: 'body'
+    })
+  ],
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+  }
+}
